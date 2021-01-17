522d540d45e969afdf72b4391a264eb355b5ea4f
Outdoor-Dev <saltairpix@gmail.com>
2021-01-17 11:06:59 +0000

    initial

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [task, setTask] = useState([
+		"Walk dog",
+		"Wash clothes",
+		"Clean house"
+	]);
+	const [userInput, setUserInput] = useState("");
+
+	const handleKeyUp = event => {
+		if (event.keyCode === 13 && userInput !== "") {
+			setTask(task.concat(userInput));
+			setUserInput("");
+		}
+	};
+	const itemDelete = index => {
+		let updatedList = task.filter(
+			(tasks, taskIndex) => index !== taskIndex
+		);
+		setTask(updatedList);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="todo-title"> Todos</h1>
+			<div className="todo-list">
+				<input
+					className="tasks"
+					type="text"
+					onChange={e => setUserInput(e.target.value)}
+					value={userInput}
+					onKeyUp={handleKeyUp}
+				/>
+
+				<ul className="list-group">
+					{task.map((value, index) => {
+						return (
+							<li className="list-group-item" key={index}>
+								{value}
+
+								<i
+									className="far fa-trash-alt"
+									onClick={() => itemDelete(index)}
+								/>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 }
